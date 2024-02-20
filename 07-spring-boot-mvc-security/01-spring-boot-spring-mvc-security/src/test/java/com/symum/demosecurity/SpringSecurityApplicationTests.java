280656ba21d247cfa37c2a57cd67e7102ef80180
Sani08 <symumsani08@gmail.com>
2024-02-20 21:51:43 +0600

    Add files via upload

---
+++ 07-spring-boot-mvc-security/01-spring-boot-spring-mvc-security/src/test/java/com/symum/demosecurity/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.symum.demosecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
